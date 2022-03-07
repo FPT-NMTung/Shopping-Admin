a7aa7774c6ad288ddb871478b4e573e4a410c392
NMTung <nmtung.study@gmail.com>
2022-03-07 19:56:27 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 function Home() {
-  const router = useRouter();
+  const router = useRouter()
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
